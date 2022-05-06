71ae78b6db11ad6a6cd27e9076ac42eb318ce46f
SugarWars <sugarwarsworks@gmail.com>
2022-05-06 23:10:51 +0900

    next.config.jsを修正

--- next.config.js
+++ next.config.js
@@ -2,16 +2,15 @@
 
 // GitHub Pages用の設定
 const urlPrefix = process.env.URL_PREFIX ? "/" + process.env.URL_PREFIX : "";
+
 const nextConfig = {
+  env: {
+    DIR: process.env.NODE_ENV == "production" ? "/studio_cording" : "",
+  },
   reactStrictMode: true,
   assetPrefix: urlPrefix, // GitHub Pages用の設定
   basePath: urlPrefix, // GitHub Pages用の設定
   trailingSlash: true, // GitHub Pages用の設定
 };
-module.exports = nextConfig;
 
-// module.exports = {
-//   env: {
-//     DIR: process.env.NODE_ENV == "production" ? "/studio_cording" : "",
-//   },
-// };
+module.exports = nextConfig;
