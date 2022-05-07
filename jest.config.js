68ae4b5e5268b753ffb1eb4417e0c9eb60a10b7f
SugarWars <sugarwarsworks@gmail.com>
2022-05-07 22:11:27 +0900

    refs #3 members.tsのテストを作成
    - getMembersData()のテストを作成
    - getMembersData()のテスト用ダミーとなる.mdファイルを作成
    - Jest用の環境変数を利用できるように設定
    - member.tsが参照するプロフィールディレクトリを環境変数から取得するように変更

--- jest.config.js
+++ jest.config.js
@@ -10,6 +10,7 @@ const customJestConfig = {
   setupFilesAfterEnv: ["<rootDir>/jest.setup.js"], // jest.setup.jsを作成する場合のみ定義。
   moduleDirectories: ["node_modules", "<rootDir>/"],
   testEnvironment: "jest-environment-jsdom",
+  globalSetup: "<rootDir>/tests/setupEnv.ts", // 環境変数の利用
 };
 
 // createJestConfigを定義することによって、本ファイルで定義された設定がNext.jsの設定に反映されます
---
+++ tests/members_sample/sample.md
@@ -0,0 +1,6 @@
+---
+name: "名前"
+year: "2022"
+image: "/images/members/sample.jpg"
+description: "自己紹介文を書いてください"
+---
